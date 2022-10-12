c597ed06d6b951fc9fa19e668fd82072ce96e7ec
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-10-13 00:14:06 +0500

    Print corresponding error and action messages

--- src/index.js
+++ src/index.js
@@ -8,17 +8,31 @@ const submit = document.querySelector(".submit");
 const input1 = document.querySelector(".input1");
 const input2 = document.querySelector(".input2");
 const refresh = document.querySelector(".refresh");
+const error = document.querySelector(".error");
 
 submit.addEventListener("click", () => {
+
+    function printmsg() {
+        error.innerHTML = ""
+    }
     
     async function additem(user, score) {
         await add(user, score);
-        
+        error.innerHTML = "Score added successfully"
+        setTimeout(printmsg, 3000)
     }
     if(input1.value !== "" && input2.value !== ""){
         additem(input1.value, input2.value);
     }
-    
+    else if (input1.value !== ""){
+        error.innerHTML = "Fill in the score"
+    }
+    else if (input2.value !== ""){
+        error.innerHTML = "Fill in the name"
+    }
+    else {
+        error.innerHTML = "Fill in the name and score"
+    }
 })
 
 refresh.addEventListener("click", () => {
