e3827e22f062134b70161c21fdf241a2beeeb281
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-10-13 00:06:33 +0500

    Add functionality of the refresh button

--- src/index.js
+++ src/index.js
@@ -2,29 +2,30 @@
 import './style.css';
 import add from './modules/addingfiles.js';
 import load from './modules/loadingfiles.js';
+import print from './modules/print.js';
 
 const submit = document.querySelector(".submit");
 const input1 = document.querySelector(".input1");
 const input2 = document.querySelector(".input2");
 const refresh = document.querySelector(".refresh");
 
-submit.addEventListener("click", (e) => {
+submit.addEventListener("click", () => {
     
     async function additem(user, score) {
         await add(user, score);
         
     }
     if(input1.value !== "" && input2.value !== ""){
-        
         additem(input1.value, input2.value);
     }
+    
 })
 
 refresh.addEventListener("click", () => {
+    
     async function loaditem() {
-        console.log("Loading started");
         const response = await load();
-        console.log("Response is", response);
+        print(response);
     }
     
     loaditem();
